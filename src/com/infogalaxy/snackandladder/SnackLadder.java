934ff662b543ec7f859e4b89e5a0125f2e7d4e18
MohammadHusainShaikh <hu62sa62@gmail.com>
2024-03-12 15:24:26 +0530

    [ADDED]:Husain - Started with single player at zero position

---
+++ src/com/infogalaxy/snackandladder/SnackLadder.java
@@ -0,0 +1,11 @@
+package com.infogalaxy.snackandladder;
+
+public class SnackLadder {
+
+	public static void main(String[] args) {
+		int pos = 0;
+		System.out.println("The position of player is "+pos);
+
+	}
+
+}
