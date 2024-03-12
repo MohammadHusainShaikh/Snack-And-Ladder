3f66927a2b19b81507612d245aac4ce35dad8460
MohammadHusainShaikh <hu62sa62@gmail.com>
2024-03-12 15:37:12 +0530

    [MODIFIED];Husain - Rolling dice using Random class

--- src/com/infogalaxy/snackandladder/SnackLadder.java
+++ src/com/infogalaxy/snackandladder/SnackLadder.java
@@ -1,11 +1,24 @@
 package com.infogalaxy.snackandladder;
 
+import java.util.Random;
 public class SnackLadder {
 
-	public static void main(String[] args) {
-		int pos = 0;
-		System.out.println("The position of player is "+pos);
-
-	}
-
+	
+		private int pos = 0;
+		public void position() {
+			System.out.println("The position of player is "+pos);
+		}
+		public void playGame() {
+		Random random = new Random();
+		int die = random.nextInt(6) + 1;
+		System.out.println("Player dies"+die);
+		
+		}
+	
+		public static void main(String[] args) {
+			SnackLadder snackLadder = new SnackLadder();
+			snackLadder.position();
+			snackLadder.playGame();
+		}
 }
+	
