3eeda3a3b69e8ceb85bee69c9c10e7d4de29e11d
MohammadHusainShaikh <hu62sa62@gmail.com>
2024-03-13 13:24:08 +0530

    [MODFIED]:Husain - Check for option STAY,LADDER or SNACK

--- src/com/infogalaxy/snackandladder/SnackLadder.java
+++ src/com/infogalaxy/snackandladder/SnackLadder.java
@@ -3,22 +3,47 @@ package com.infogalaxy.snackandladder;
 import java.util.Random;
 public class SnackLadder {
 
-	
-		private int pos = 0;
-		public void position() {
-			System.out.println("The position of player is "+pos);
-		}
-		public void playGame() {
 		Random random = new Random();
-		int die = random.nextInt(6) + 1;
-		System.out.println("Player dies"+die);
+
+		private int playerPosition = 0;
+		
+		private int option;
+		private static final int STAY = 0;
+		private static final int LADDER = 1;
+		private static final int SNAKE = 2;
+
+		public void playGame() {
+		System.out.println("The position of player is "+playerPosition);
+		int dieNo = random.nextInt(6) + 1;
+		option = random.nextInt(3); 
+		
+		switch(option) {
+		case STAY:
+			System.out.println("STAY "+option);
+			
+			break;
+		case LADDER:
+			System.out.println("LADDER "+option);
+			playerPosition += dieNo;
+			break;
+		case SNAKE:
+			System.out.println("SNAKE "+option);
+			playerPosition -= dieNo;
+			if(playerPosition < 0) {
+				playerPosition = 0;
+				break;
+			}
+
+		}
+		
+		System.out.println("Number of die "+dieNo);
+		System.out.println("The new position of a player is "+playerPosition);
 		
 		}
 	
 		public static void main(String[] args) {
-			SnackLadder snackLadder = new SnackLadder();
-			snackLadder.position();
-			snackLadder.playGame();
+			SnackLadder player = new SnackLadder();
+			player.playGame();
 		}
 }
 	
